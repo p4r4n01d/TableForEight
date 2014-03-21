943e2bd83a1724c37960b99fd98c858b0eac4ce1
p4r4n01d <p4r4n01d42@gmail.com>
2014-03-21 15:27:39 +1100

    DB Validations; add update to votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,14 @@
 class VotesController < ApplicationController
 
-  before_filter :get_event
+  before_filter :get_event only :index, :show, :create
+  before_filter :get_vote only :destroy, :update
 
   def get_event
     @event = Event.find(params[:event_id])
+  end
+
+  def get_vote
+    @vote = Vote.find(params[:id])
   end
 
   # GET /events/:event_id/votes/:id
@@ -30,12 +35,23 @@ class VotesController < ApplicationController
 
   # DELETE /votes/:vote_id
   def destroy
-    @vote = Vote.find(params[:id])
     respond_to do |format|
       if @vote.destroy
         format.json { head :no_content, status: :ok }
       else
         format.json { render json: @vote.errors, status: :unprocessable_entity }
       end
+  end
+
+
+  # PUT /votes/:vote_id
+  def update
+    respond_to do |format|
+      if @vote.update_attributes(params[:vote])
+        format.json { head :no_content, status: :ok }
+      else
+        format.json { render json: @vote.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ActiveRecord::Base
-	has_many :votes	
+  has_many :votes
+
+  validates_presence_of :link1
+  validates_presence_of :date1
+  validates_presence_of :organiser_email
 end
