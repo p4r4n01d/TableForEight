7c36e3a49db52daa20639c52ac55138d0e792f2c
jsthink <julian@jsthink.com>
2014-03-27 13:07:24 +1100

    JS

--- app/assets/javascripts/ctrl.js
+++ app/assets/javascripts/ctrl.js
@@ -30,8 +30,9 @@ function eventCtrl($scope, $http, $templateCache) {
 		{ "id":vote_id,
 		 type:result }
 		};
+		VoteJsonNew = null;
 		//update votes controller to return JSONInterface format result.events.id
-		VoteJsonNew=JSONInterface("http://localhost:3000/api/events/"+event_id+"/"+vote_id+"", VoteJSON, "POST", "ok");
+		//VoteJsonNew=JSONInterface("http://localhost:3000/api/events/"+event_id+"/"+vote_id+"", VoteJSON, "POST", "ok");
 		alert("VoteID:" + VoteJsonNew);
 		tempVote_resutl="";
 		if(VoteJsonNew!=null) tempVote_resutl = "Thank You for Voting";
