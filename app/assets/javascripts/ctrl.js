42bb9a283058bd895533066a23f460cfea3c2c3c
jsthink <julian@jsthink.com>
2014-03-28 00:32:16 +1100

    Results & JS

--- app/assets/javascripts/ctrl.js
+++ app/assets/javascripts/ctrl.js
@@ -23,95 +23,6 @@ function JSONInterface(url, jsObj, type, get_status) {
 function eventCtrl($scope, $http, $templateCache) {
 	
 	$scope.method = 'GET';
-	$scope.test_resutl = function() {
-		Result_json = {"voting":{"link1":{"going":{"count":[{"Counts":2,"id":null}],"emails":[{"email":"jeromerrodriguez@gmail.com","id":null},{"email":"justindennis2000@hotmail.com","id":null}]},"notgoing":{"count":[{"Counts":0,"id":null}],"emails":[]}},"link2":{"going":{"count":[{"Counts":0,"id":null}],"emails":[]},"notgoing":{"count":[{"Counts":2,"id":null}],"emails":[{"email":"jeromerrodriguez@gmail.com","id":null},{"email":"justindennis2000@hotmail.com","id":null}]}},"link3":{"going":{"count":[{"Counts":0,"id":null}],"emails":[]},"notgoing":{"count":[{"Counts":2,"id":null}],"emails":[{"email":"jeromerrodriguez@gmail.com","id":null},{"email":"justindennis2000@hotmail.com","id":null}]}},"link4":{"going":{"count":[{"Counts":0,"id":null}],"emails":[]},"notgoing":{"count":[{"Counts":2,"id":null}],"emails":[{"email":"jeromerrodriguez@gmail.com","id":null},{"email":"justindennis2000@hotmail.com","id":null}]}},"link5":{"going":{"count":[{"Counts":0,"id":null}],"emails":[]},"notgoing":{"count":[{"Counts":2,"id":null}],"emails":[{"email":"jeromerrodriguez@gmail.com","id":null},{"email":"justindennis2000@hotmail.com","id":null}]}},"date1":{"going":{"count":[{"Counts":0,"id":null}],"emails":[]},"notgoing":{"count":[{"Counts":2,"id":null}],"emails":[{"email":"jeromerrodriguez@gmail.com","id":null},{"email":"justindennis2000@hotmail.com","id":null}]},"maybe":{"count":[{"Counts":0,"id":null}],"emails":[]}},"date2":{"going":{"count":[{"Counts":0,"id":null}],"emails":[]},"notgoing":{"count":[{"Counts":2,"id":null}],"emails":[{"email":"jeromerrodriguez@gmail.com","id":null},{"email":"justindennis2000@hotmail.com","id":null}]},"maybe":{"count":[{"Counts":0,"id":null}],"emails":[]}},"date3":{"going":{"count":[{"Counts":0,"id":null}],"emails":[]},"notgoing":{"count":[{"Counts":2,"id":null}],"emails":[{"email":"jeromerrodriguez@gmail.com","id":null},{"email":"justindennis2000@hotmail.com","id":null}]},"maybe":{"count":[{"Counts":0,"id":null}],"emails":[]}}}};
-		alert("date3 Count (going: "+ JSON.stringify(Result_json.voting['link1'].going.count)+"::::::::: not goint: "+JSON.stringify(Result_json.voting['link1'].notgoing.count)+")");
-		for(link_count=1;link_count<6;link_count++)
-		{
-			link_title="link"+link_count;
-			for(b = 0; b < 2; b++)
-			{
-				tempObj=null;
-				tempObj1=null;
-				scopeCount="";
-				divPlace="";
-				switch(b){
-					case 0:
-					tempObj= Result_json.voting[link_title].going.count;
-					tempObj1= Result_json.voting[link_title].going.emails;
-					scopeCount=link_title+'going';
-					divPlace="#linkbtnyes"+link_count;
-					break;
-					case 1:
-					tempObj= Result_json.voting[link_title].notgoing.count;
-					tempObj1= Result_json.voting[link_title].notgoing.emails;
-					scopeCount=link_title+'notgoing';
-					divPlace="#linkbtnno"+link_count;
-					break;
-				}
-				for(i = 0; i < tempObj.length; i++)
-				{
-					objYes = tempObj[i];
-					$scope[scopeCount] = objYes.Counts;
-					if(objYes.Counts>0)
-					{
-						//alert("link count="+link_count+":::b="+b);
-						var tempDate="";
-						for(a=0; a<tempObj1.length;a++){
-							object4 = tempObj1[a];
-							tempDate+=object4.email+"<br />";
-						}
-						//$(divPlace).attr("data-content",tempDate).data('popover').setContent();
-					}
-				}
-			}
-		}
-		for(link_count=1;link_count<4;link_count++)
-		{
-			link_title="date"+link_count;
-			for(b = 0; b < 3; b++)
-			{
-				tempObj=null;
-				tempObj1=null;
-				scopeCount="";
-				divPlace="";
-				switch(b){
-					case 0:
-					tempObj= Result_json.voting[link_title].going.count;
-					tempObj1= Result_json.voting[link_title].going.emails;
-					scopeCount=link_title+'going';
-					divPlace="#datebtnyes"+link_count;
-					break;
-					case 1:
-					tempObj= Result_json.voting[link_title].notgoing.count;
-					tempObj1= Result_json.voting[link_title].notgoing.emails;
-					scopeCount=link_title+'notgoing';
-					divPlace="#datebtnno"+link_count;
-					break;
-					case 2:
-					tempObj= Result_json.voting[link_title].maybe.count;
-					tempObj1= Result_json.voting[link_title].maybe.emails;
-					scopeCount=link_title+'maybe';
-					divPlace="#datebtnmaybe"+link_count;
-					break;
-				}
-				for(i = 0; i < tempObj.length; i++)
-				{
-					objYes = tempObj[i];
-					$scope[scopeCount] = objYes.Counts;
-					if(objYes.Counts>0)
-					{
-						var tempDate="";
-						for(a=0; a<tempObj1.length;a++){
-							object4 = tempObj1[a];
-							tempDate+=object4.email+"<br />";
-						}
-						//$(divPlace).attr("data-content",tempDate).data('popover').setContent();
-					}
-				}
-			}
-		}
-	};
 	 
 	$scope.vote_process = function(event_id, vote_id, type, result) {
 		VoteJSON = {
@@ -137,6 +48,130 @@ function eventCtrl($scope, $http, $templateCache) {
 				$scope.voteResult_confirm = tempVote_resutl;
 			break;
 		}
+	};
+	 
+	$scope.selcetion = function(event_id, vote_id, type, result) {
+		VoteJSON = {
+		"vote": 
+		{ "id":vote_id }
+		};
+		VoteJSON.vote[type]=result;
+		VoteJsonNew = null;
+		VoteJsonNew=JSONInterface("http://localhost:3000/api/events/"+event_id+"/"+vote_id+"", VoteJSON, "POST", "ok");
+		alert("VoteID:" + VoteJsonNew);
+		tempVote_resutl="";
+		if(VoteJsonNew!=null) tempVote_resutl = "Thank You for Voting";
+		else tempVote_resutl = "Vote Not Set";
+		
+		switch(type){
+			case "link":
+				$scope.voteResult_where = tempVote_resutl;
+			break;
+			case "date":
+				$scope.voteResult_when = tempVote_resutl;
+			break;
+			case "confirmed":
+				$scope.voteResult_confirm = tempVote_resutl;
+			break;
+		}
+	};
+	$scope.results = function(event_id) {
+		Result_json = "";
+		$http.get('/api/get/'+event_id).
+		success(function(response) {
+			Result_json=response;
+			if(Result_json!=""){
+				for(link_count=1;link_count<6;link_count++)
+				{
+					link_title="link"+link_count;
+					for(b = 0; b < 2; b++)
+					{
+						tempObj=null;
+						tempObj1=null;
+						scopeCount="";
+						divPlace="";
+						switch(b){
+							case 0:
+							tempObj= Result_json.voting[link_title].going.count;
+							tempObj1= Result_json.voting[link_title].going.emails;
+							scopeCount=link_title+'going';
+							divPlace="#linkbtnyes"+link_count;
+							break;
+							case 1:
+							tempObj= Result_json.voting[link_title].notgoing.count;
+							tempObj1= Result_json.voting[link_title].notgoing.emails;
+							scopeCount=link_title+'notgoing';
+							divPlace="#linkbtnno"+link_count;
+							break;
+						}
+						for(i = 0; i < tempObj.length; i++)
+						{
+							objYes = tempObj[i];
+							$scope[scopeCount] = objYes.Counts;
+							if(objYes.Counts>0)
+							{
+								var tempDate="None";
+								divDetails=$(divPlace);
+								for(a=0; a<tempObj1.length;a++){
+									object4 = tempObj1[a];
+									tempDate+=object4.email+"<br />";
+								}
+								divDetails.attr("data-content",tempDate);
+							}
+						}
+					}
+				}
+				for(link_count=1;link_count<4;link_count++)
+				{
+					link_title="date"+link_count;
+					for(b = 0; b < 3; b++)
+					{
+						tempObj=null;
+						tempObj1=null;
+						scopeCount="";
+						divPlace="";
+						switch(b){
+							case 0:
+							tempObj= Result_json.voting[link_title].going.count;
+							tempObj1= Result_json.voting[link_title].going.emails;
+							scopeCount=link_title+'going';
+							divPlace="#datebtnyes"+link_count;
+							break;
+							case 1:
+							tempObj= Result_json.voting[link_title].notgoing.count;
+							tempObj1= Result_json.voting[link_title].notgoing.emails;
+							scopeCount=link_title+'notgoing';
+							divPlace="#datebtnno"+link_count;
+							break;
+							case 2:
+							tempObj= Result_json.voting[link_title].maybe.count;
+							tempObj1= Result_json.voting[link_title].maybe.emails;
+							scopeCount=link_title+'maybe';
+							divPlace="#datebtnmaybe"+link_count;
+							break;
+						}
+						for(i = 0; i < tempObj.length; i++)
+						{
+							objYes = tempObj[i];
+							$scope[scopeCount] = objYes.Counts;
+							if(objYes.Counts>0)
+							{
+								var tempDate="None";
+								divDetails=$(divPlace);
+								for(a=0; a<tempObj1.length;a++){
+									object4 = tempObj1[a];
+									tempDate+=object4.email+"<br />";
+								}
+								divDetails.attr("data-content",tempDate);
+							}
+						}
+					}
+				}
+			}
+		}).
+		error(function(data, status) {
+			$scope.selectedRestaurant= data || "Request failed";
+		});
 	};
 	 
 	$scope.fetch = function(titleURL, restaurantTitle) {
