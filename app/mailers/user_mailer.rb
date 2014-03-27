8468ea93a3ce73b980a050e1e91ff2ac90d1efad
jsthink <julian@jsthink.com>
2014-03-28 09:35:41 +1100

    Mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,10 +8,17 @@ class UserMailer < ActionMailer::Base
          subject: "You Have been Invited")
   end
   
-  def confirm_email(user, event)
+  def admin_email(user, event)
     @event = event
     @user = user
     mail(to: @user.email,
+         subject: "Event Date and Time Confirmed")
+  end
+  
+  def confirm_email(user, event)
+    @event = event
+    @user = user
+    mail(to: @user,
          subject: "Event Date and Time Confirmed")
   end
 end
