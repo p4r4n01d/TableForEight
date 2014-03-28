d530d8e11a7cb850205cff64bcaf516b1088ea18
jsthink <julian@jsthink.com>
2014-03-28 16:16:04 +1100

    Assets Compilling retry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ TableForEight::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w(*.js *.css *.eot, *.svg, *.ttf *.woff)
+  # config.assets.precompile += %w(*.js *.css *.eot, *.svg, *.ttf *.woff)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
