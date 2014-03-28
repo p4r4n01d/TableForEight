9bcd744a23052e38733625d7bf520b206a439647
p4r4n01d <p4r4n01d42@gmail.com>
2014-03-28 16:27:02 +1100

    Refactor date buttons in voting page
    Remove unused event test page

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,25 @@
 module VotesHelper
+
+  def dateButton(value)
+    str = "<button type=\"button\" class=\"btn btn-default\" ng-click=\"vote_process(#{@events.id}, #{@votes.id}, 'date#{i}', #{value})")\">"
+    spanclass = nil
+
+    case value
+      when -1
+        str += "No"
+        spanclass = "down"
+      when 0
+        str += "Maybe"
+      when 1
+        str += "Yes"
+        spanclass = "up"
+      end
+    
+    str += text
+    if !!spanClass
+      str += <span class="glyphicon glyphicon-thumbs-#{spanclass}"></span>
+    end
+    raw(str + "</button>")
+  end
+
 end
