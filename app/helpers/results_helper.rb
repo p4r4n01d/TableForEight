faaac2ef504c6065d9c6ae45a15b9fa31068a575
p4r4n01d <p4r4n01d42@gmail.com>
2014-03-31 14:03:18 +1100

    Refactor organiser's voting page buttons
    Re-arrange vote buttons

---
+++ app/helpers/results_helper.rb
@@ -0,0 +1,45 @@
+module ResultsHelper
+
+  # Button to vote for a restaurant or time
+  # value: The numeric value associated with this button
+  # i: The number of this button in sequence
+  # type: What this button is for 
+  def results_voteButton(value, i, type)
+    
+    templating = ""
+    spanClass = nil
+    case value
+      when -1
+        title = "No"
+        spanClass = "down"
+        templating = "notgoing"
+        id = "no"
+      when 0
+        title = "Not sure"
+        templating = "maybe"
+        id ="maybe"
+        spanClass = "up"
+      when 1
+        title = "Yes"
+        spanClass = "up"
+        templating = "going"
+        id = "yes"
+      end
+
+    str = "<button type=\"button\" id=\"#{type}btn#{id}#{i}\" data-container=\"body\""
+    str += " data-toggle=\"popover\" data-trigger=\"hover\" data-placement=\"bottom\""
+
+    str += " class=\"btn btn-default btn-sm\" data-content=\"None\" data-original-title=\"#{title}:\">\n"
+
+    str += "{{ #{type}#{i}#{templating} }}\n"
+
+    if !!spanClass
+      str += "<span class=\"glyphicon glyphicon-thumbs-#{spanClass}\"></span>\n"
+    end
+    raw(str + "</button>")
+  end
+end
+
+									
+									
+
