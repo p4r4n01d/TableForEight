24ebce13df38ef55974695487930fd5b28038824
jsthink <julian@jsthink.com>
2014-03-31 12:33:32 +1100

    Getting Production Email Errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,12 +57,13 @@ TableForEight::Application.configure do
   config.action_controller.asset_host = "http://tableforeight.herokuapp.com"
 
 config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
     :address => "smtp.mandrillapp.com",
     :port => 587,
     :user_name => "app23461406@heroku.com",
-    :password => "KfbzKJXZtNtYG0OsxePHNg"
+    :password => "E-CbxA-6lCPr5weoU07m8Q"
 }
 
   # Precompile additional assets.
