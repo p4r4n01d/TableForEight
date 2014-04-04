f03a3ffa8639b0198ded65c16bcde6e07cc5cf2f
p4r4n01d <p4r4n01d42@gmail.com>
2014-04-04 16:29:13 +1100

    Add facebook like box

--- app/assets/javascripts/social.js
+++ app/assets/javascripts/social.js
@@ -32,3 +32,12 @@ hello.on('auth.login', function(auth) {
         });
     });
 });
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=327986827326660";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
